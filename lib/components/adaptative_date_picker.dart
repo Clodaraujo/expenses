ad7f489e07ea1a528958b8f0d65d7e82c478dd4f
Clodaraujo <clodoaldo.araujos@gmail.com>
2020-10-04 21:04:43 -0300

    fisrt commit

---
+++ lib/components/adaptative_date_picker.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
+
+
+class AdaptativeDatePicker extends StatelessWidget {
+
+  final DateTime selectedDate;
+  final Function (DateTime) onDateChanged;
+ 
+
+  AdaptativeDatePicker({
+    this.selectedDate,
+    this.onDateChanged,
+  });
+
+  _showDatePicker(BuildContext context) {
+    showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2019),
+      lastDate: DateTime.now(),
+    ).then((pickedDate) {
+      if (pickedDate == null) {
+        return;
+      }
+      onDateChanged(pickedDate);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? Container(
+          height: 180,
+          child: CupertinoDatePicker(
+            mode:CupertinoDatePickerMode.date,
+            initialDateTime: DateTime.now(),
+            minimumDate: DateTime(2019),
+            maximumDate: DateTime.now(),
+            onDateTimeChanged: onDateChanged,
+          ),
+        )
+        : Container(
+            height: 75,
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: Text(
+                    selectedDate == null
+                        ? 'Nenhuma data selecionada!'
+                        : 'Data Selecionada :${DateFormat('dd/MM/y').format(selectedDate)}',
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    RaisedButton(
+                      child: Text('Selecionar data'),
+                      color: Theme.of(context).primaryColor,
+                      textColor: Theme.of(context).textTheme.button.color,
+                      onPressed: () => _showDatePicker (context),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+  }
+}
---
+++ lib/components/transaction_list.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'Transaction_Item.dart';
+import '../models/transaction.dart';
+import 'package:flutter/cupertino.dart';
+
+class TransactionList extends StatelessWidget {
+  final List<Transaction> transactions;
+  final void Function(String) onRemove;
+
+  TransactionList(this.transactions, this.onRemove);
+
+  @override
+  Widget build(BuildContext context) {
+    return transactions.isEmpty
+        ? LayoutBuilder(
+            builder: (ctx, constraints) {
+              return Column(
+                children: <Widget>[
+                  SizedBox(height: 20),
+                  Text(
+                    'Nenhuma Transaçao Cadastrada',
+                    style: Theme.of(context).textTheme.headline6,
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
+                    ),
+                  )
+                ],
+              );
+            },
+          )
+        // :
+        // ListView(
+        //   children:transactions.map((tr) {
+        //     return TransactionItem(
+        //       key: ValueKey(tr.id),
+        //       tr: tr,
+        //       onRemove: onRemove,
+        //     );
+        //   }).toList(),
+        //   );
+        : ListView.builder(
+            itemCount: transactions.length,
+            itemBuilder: (ctx, index) {
+              final tr = transactions[index];
+              return TransactionItem(
+                key: GlobalObjectKey(tr),
+                tr: tr,
+                onRemove: onRemove,
+              );
+            },
+          );
+  }
+}
